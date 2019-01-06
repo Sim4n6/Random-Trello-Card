c3e401ac32f17f9cfc2bb1360d55eb44ebc92bc8
Sim4n6 <sim4n6@gmail.com>
2019-01-06 09:47:03 +0100

    Flasked the app

--- Random-Trello-Card.py
+++ Random-Trello-Card.py
@@ -1,5 +1,6 @@
 from trello import TrelloClient
 import random
+from flask import Flask, render_template_string
 
 # My API KEY
 from keys import TRELLO_APP_KEY, TRELLO_USER_TOKEN, API_SECRET
@@ -11,21 +12,29 @@ client = TrelloClient(
 	token_secret='your-oauth-token-secret'
 )
 
-if __name__ == '__main__':
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
 	# list trello boards
 	lst_boards = client.list_boards()
-	print(lst_boards)
 
 	# list of the TODOS Boards
-	todos_boards = lst_boards[1]
-	lst_brd_lists = todos_boards.list_lists()
-	print(lst_brd_lists)
+	todos_board = lst_boards[1]
+	lst_brd_lists = todos_board.list_lists()
 
 	# list cards of TODOS board
-	To_dos_lst_cards = lst_brd_lists[0]
-	lst_cards = To_dos_lst_cards.list_cards()
-	print(lst_cards)
+	todos_lst_cards = lst_brd_lists[0]
+	lst_cards = todos_lst_cards.list_cards()
 
 	# Random card of the list
 	random_card = random.choice(lst_cards)
-	print("-->", random_card.name)
+	return render_template_string(
+		"""<a href='{{ random_card.url }}'>{{ random_card.name }}</a>""", random_card=random_card)
+
+
+if __name__ == '__main__':
+
+	app.config['ENV'] = "development"
+	app.run(host="127.0.0.1", port=5000, debug=True)
