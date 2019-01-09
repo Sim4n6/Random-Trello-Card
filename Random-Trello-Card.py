13df868542cb5ff440cb7a288e8d5d99f8b6d88c
Sim4n6 <Sim4n6@gmail.com>
2019-01-09 10:39:25 +0000

    added support for random extraction of saved article from GetPocket using Pocket API v3

--- Random-Trello-Card.py
+++ Random-Trello-Card.py
@@ -1,6 +1,9 @@
 from trello import TrelloClient
 import random
 from flask import Flask, render_template
+import requests
+import json
+import subprocess
 
 # My API KEY
 from keys import TRELLO_APP_KEY, TRELLO_USER_TOKEN, API_SECRET
@@ -13,6 +16,46 @@ client = TrelloClient(
 )
 
 app = Flask(__name__)
+
+
+def get_pocket():
+
+	# Retrieve request_token
+	r2 = requests.get('https://getpocket.com/v3/oauth/request?consumer_key=83185-696ae1741f775407425ba954&redirect_uri=MyPocket123:authorizationFinished')
+	code = r2.content
+	request_token = str(code).split("=")[1].strip("'")
+	print("request_token: ", request_token)
+
+	# Authorization de lapp a mon compte
+	r = requests.get('https://getpocket.com/auth/authorize?request_token=' + request_token + '&redirect_uri=MyPocket123:authorizationFinished')
+	print(r.url)
+	code = subprocess.call(["C:\Program Files (x86)\Google\Chrome\Application\chrome.exe", r.url])
+	if code:
+		print(code)
+	else:
+		print(">" + str(code))
+
+	# get access token
+	r3 = requests.get('https://getpocket.com/v3/oauth/authorize?consumer_key=83185-696ae1741f775407425ba954&code=' + request_token)
+	code = r3.content
+	access_token = str(code).split("=")[1].split("&")[0]
+	print("access_token", access_token)
+
+	# Get all data
+	r4 = requests.get('https://getpocket.com/v3/get?consumer_key=83185-696ae1741f775407425ba954&access_token=' + access_token)
+	code = r4.content
+	print(">>>", code)
+
+	# loads json data
+	loaded_json = json.loads(code)
+	print(loaded_json["list"]['2341781570']['given_url'])
+
+	# random saved post
+	list_saved = [k for k in loaded_json["list"].keys()]
+	random_saved_key = random.choice(list_saved)
+	print(">>>>>>", random_saved_key)
+
+	return loaded_json["list"][random_saved_key]['given_url']
 
 
 @app.route("/")
@@ -31,8 +74,11 @@ def index():
 	# Generate a random card of the list
 	random_card = random.choice(lst_cards)
 
+	# Get Pocket informations:
+	pocket_url = get_pocket()
+
 	# render a template with random_card details
-	return render_template("index.html", random_card=random_card)
+	return render_template("index.html", random_card=random_card, pocket_url=pocket_url)
 
 
 if __name__ == '__main__':
