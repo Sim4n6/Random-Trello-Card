4768eb6fbdb699b6f8b6f138bf2de03b43ca4334
Sim4n6 <Sim4n6@gmail.com>
2019-01-09 10:41:59 +0000

    removed some prints

--- Random-Trello-Card.py
+++ Random-Trello-Card.py
@@ -24,36 +24,32 @@ def get_pocket():
 	r2 = requests.get('https://getpocket.com/v3/oauth/request?consumer_key=83185-696ae1741f775407425ba954&redirect_uri=MyPocket123:authorizationFinished')
 	code = r2.content
 	request_token = str(code).split("=")[1].strip("'")
-	print("request_token: ", request_token)
+	#print("request_token: ", request_token)
 
 	# Authorization de lapp a mon compte
 	r = requests.get('https://getpocket.com/auth/authorize?request_token=' + request_token + '&redirect_uri=MyPocket123:authorizationFinished')
-	print(r.url)
-	code = subprocess.call(["C:\Program Files (x86)\Google\Chrome\Application\chrome.exe", r.url])
-	if code:
-		print(code)
-	else:
-		print(">" + str(code))
+	#print(r.url)
+#	code = subprocess.call(["C:\Program Files (x86)\Google\Chrome\Application\chrome.exe", r.url])
 
 	# get access token
 	r3 = requests.get('https://getpocket.com/v3/oauth/authorize?consumer_key=83185-696ae1741f775407425ba954&code=' + request_token)
 	code = r3.content
 	access_token = str(code).split("=")[1].split("&")[0]
-	print("access_token", access_token)
+#	print("access_token", access_token)
 
 	# Get all data
 	r4 = requests.get('https://getpocket.com/v3/get?consumer_key=83185-696ae1741f775407425ba954&access_token=' + access_token)
 	code = r4.content
-	print(">>>", code)
+#	print(">>>", code)
 
 	# loads json data
 	loaded_json = json.loads(code)
-	print(loaded_json["list"]['2341781570']['given_url'])
+#	print(loaded_json["list"]['2341781570']['given_url'])
 
 	# random saved post
 	list_saved = [k for k in loaded_json["list"].keys()]
 	random_saved_key = random.choice(list_saved)
-	print(">>>>>>", random_saved_key)
+#	print(">>>>>>", random_saved_key)
 
 	return loaded_json["list"][random_saved_key]['given_url']
 
