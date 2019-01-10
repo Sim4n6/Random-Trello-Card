5e42c55de8cc18dc98d217d425bf1fae8dfab394
Sim4n6 <Sim4n6@gmail.com>
2019-01-10 13:33:55 +0000

    some code formatting and method extraction

--- Random-Trello-Card.py
+++ Random-Trello-Card.py
@@ -1,16 +1,18 @@
+import json
 from trello import TrelloClient
 import random
 from flask import Flask, render_template
 import requests
-import json
-import subprocess
 
-# My API KEY
-from keys import TRELLO_APP_KEY, TRELLO_USER_TOKEN, API_SECRET, CONSUMER_KEY, ACCESS_TOKEN
+# My TRELLO API KEYs
+from keys import TRELLO_APP_KEY, TRELLO_USER_TOKEN, TRELLO_API_SECRET
 
-client = TrelloClient(
+# My POCKET API KEYs
+from keys import CONSUMER_KEY, ACCESS_TOKEN
+
+client_trello = TrelloClient(
 	api_key=TRELLO_APP_KEY,
-	api_secret=API_SECRET,
+	api_secret=TRELLO_API_SECRET,
 	token=TRELLO_USER_TOKEN,
 	token_secret='your-oauth-token-secret'
 )
@@ -18,9 +20,8 @@ client = TrelloClient(
 app = Flask(__name__)
 
 
-def get_pocket():
-
-
+def get_random_pocket():
+	""" Get a Random Pocket post """
 
 	# # Retrieve request_token
 	# r2 = requests.get('https://getpocket.com/v3/oauth/request?consumer_key=' + consumer_key + '&redirect_uri=MyPocket123:authorizationFinished')
@@ -47,7 +48,6 @@ def get_pocket():
 	# Get all data
 	r4 = requests.get('https://getpocket.com/v3/get?consumer_key=' + CONSUMER_KEY + '&access_token=' + ACCESS_TOKEN)
 	code = r4.content
-	print(">>>", code)
 
 	# loads json data
 	loaded_json = json.loads(code)
@@ -55,15 +55,15 @@ def get_pocket():
 	# random saved post
 	list_saved = [k for k in loaded_json["list"].keys()]
 	random_saved_key = random.choice(list_saved)
-	print(">>>>>>", random_saved_key)
 
 	return loaded_json["list"][random_saved_key]['given_url']
 
 
-@app.route("/")
-def index():
+def get_random_trello_card():
+	""" Get a random card from Trello : Board (TODOS) --> LIST (0)"""
+
 	# list trello boards
-	lst_boards = client.list_boards()
+	lst_boards = client_trello.list_boards()
 
 	# list of the TODOS Boards
 	todos_board = lst_boards[1]
@@ -76,14 +76,21 @@ def index():
 	# Generate a random card of the list
 	random_card = random.choice(lst_cards)
 
-	# Get Pocket informations:
-	pocket_url = get_pocket()
+	return random_card
+
+
+@app.route("/")
+def index():
+	# Get Random Trello Card :
+	random_card = get_random_trello_card()
+
+	# Get Random Pocket informations:
+	pocket_url = get_random_pocket()
 
 	# render a template with random_card details
 	return render_template("index.html", random_card=random_card, pocket_url=pocket_url)
 
 
 if __name__ == '__main__':
-
 	app.config['ENV'] = "development"
 	app.run(debug=True)
